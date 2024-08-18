ed58e8c8201c75e470cc3758c2a8da186fc5bf94
Ken Böckler <estken@gmail.com>
2024-08-19 00:31:22 +0300

    cloud working

--- src/pages/RoutesPage.jsx
+++ src/pages/RoutesPage.jsx
@@ -3,7 +3,7 @@ import { useLocation, Link } from 'react-router-dom';
 
 function RoutesPage() {
     // const apiUrl = "http://localhost:9090";
-    const apiUrl = "cosmos-odyssey-core-ekf4hac5hweqcmhz.northeurope-01.azurewebsites.net";
+    const apiUrl = "https://cosmos-odyssey-core-ekf4hac5hweqcmhz.northeurope-01.azurewebsites.net";
     // const apiUrl = import.meta.env.VITE_API_URL;
 
     const [routes, setRoutes] = useState([]);
