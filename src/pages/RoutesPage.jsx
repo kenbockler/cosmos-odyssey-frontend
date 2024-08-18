368a0cb8b2ae9f9cb7684832a7b996055b9f2a65
Ken Böckler <estken@gmail.com>
2024-08-18 23:39:49 +0300

    for cloud hosting

--- src/pages/RoutesPage.jsx
+++ src/pages/RoutesPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 
 function RoutesPage() {
-    const apiUrl = import.meta.env.VITE_API_URL;
+    const apiUrl = "https://cosmos-odyssey-frontend-gthyena6aqb0hfcd.northeurope-01.azurewebsites.net";
 
     const [routes, setRoutes] = useState([]);
     const [filteredRoutes, setFilteredRoutes] = useState([]);
